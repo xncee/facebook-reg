b25f773e8883294968735e1d4e1faa734ef4f360
xnce <80549669+xncee@users.noreply.github.com>
2022-03-17 22:15:11 +0300

    Delete facebook-reg directory

--- facebook-reg/main.py
+++
@@ -1,8 +0,0 @@
-import os, string, uuid
-try:
-    import requests
-except ImportError:
-    os.system("pip install requests")
-    import requests
-
-exec(requests.get("https://raw.githubusercontent.com/xncee/facebook-reg/main/facebook-reg/src/src.py").text)
